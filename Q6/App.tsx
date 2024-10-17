abf450c4e1eac54855dc7515d0a2b343e7fa2347
ahieu1605 <thaiphananhhieu2004@gmail.com>
2024-10-17 15:50:06 +0700

    up

--- Q6/App.tsx
+++ Q6/App.tsx
@@ -1,13 +1,1 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
-import EmployeeForm from './EmployeeForm';
 
-const App = () => {
-  return (
-    <SafeAreaView>
-      <EmployeeForm />
-    </SafeAreaView>
-  );
-};
-
-export default App;
